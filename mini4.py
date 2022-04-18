19383550f7e065af7d346d4eab46ef54002f4f8b
rudrkrishna <Kittu2909@>
2022-04-18 12:18:13 +0530

    4th mini assignmnet

--- mini4.py
+++ mini4.py
@@ -5,8 +5,8 @@ eqn = lambda a, b, c, x: print(a*x*x+b*x+c)
 eqn(1, 2, 3, 4)
 
 #count of occurance of Letters
+
 lst1=["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
-liCount=[]
 
 letterCount = lambda stri, c: str(stri.count(c))
 def counterFun(li):
